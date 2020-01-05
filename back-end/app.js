43ecf29a54606313617a69b63e2e2b07e60520f2
MikePursuit <michaelamparo@pursuit.org>
2020-01-05 14:38:42 -0500

    trying to locate database

--- back-end/app.js
+++ back-end/app.js
@@ -1,20 +1,55 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+const multer = require('multer');
 
-var app = express();
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images')
+    }, 
 
+    filename: (req, file, cb) => {
+        let name = Date.now() + "-" + file.originalname
+        cb(null,name)
+    }
+})
+​
+const upload = multer({storage: storage});
+
+const homeRouter = require('./routes/home');
+const usersRouter = require('./routes/users');
+const photosRouter = require('./routes/photos');
+const captionsRouter = require('./routes/captions');
+const likesRouter = require('./routes/likes');
+const app = express();
+
+app.use(cors)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+​
 
-app.use('/', indexRouter);
+app.use('/home', homeRouter);
 app.use('/users', usersRouter);
+app.use('/photos', photosRouter);
+app.use('/captions', captionsRouter);
+app.use('/likes', likesRouter);
+​
+app.post('/upload', upload.single("image"), (req, res, next) => {
+    console.log('req.file', req.file)
+    console.log('req.body',req.body) 
 
+    let imageUrl = "http:localhost:3001/" + req.file.path.replace('public/', '')
+
+    res.json({ 
+        imageUrl: imageUrl,
+        message: 'file recieved, image was uploaded to images folder.'
+    })
+})
+​
 module.exports = app;
---
+++ back-end/memedb.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS memedb;
+
+CREATE DATABASE memedb;
+
+\c memedb;
+
+
+CREATE TABLE users( 
+    id SERIAL PRIMARY KEY,
+    email VARCHAR, 
+    firstname VARCHAR,
+    displayname VARCHAR,
+    bio VARCHAR,
+    profilePic VARCHAR,
+    active BOOLEAN 
+);
+
+
+CREATE TABLE photos (
+    id SERIAL PRIMARY KEY,
+    userId INT REFERENCES users (id),
+    picture_url VARCHAR, 
+    date_posted VARCHAR,
+    active BOOLEAN 
+);
+
+
+CREATE TABLE captions (
+    id SERIAL PRIMARY KEY,
+    photo_id INT REFERENCES photos (id),
+    commenter_id INT REFERENCES users (id),
+    body VARCHAR,
+    active BOOLEAN;
+);
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    like_value BOOLEAN, 
+    liker_id INT REFERENCES users (id),
+    caption_id INT REFERENCES captions (id),
+    active BOOLEAN
+);
+
+
