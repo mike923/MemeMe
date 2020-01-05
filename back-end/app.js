9bc1258ee726fa7728f35281d5470b5055ec32fe
aransagarcia <aransagarcia@pursuit.org>
2020-01-05 15:26:13 -0500

    created gitignore

--- back-end/app.js
+++ back-end/app.js
@@ -3,21 +3,21 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors')
-const multer = require('multer');
+// const cors = require('cors')
+// const multer = require('multer');
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/images')
-    }, 
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, './public/images')
+//     }, 
 
-    filename: (req, file, cb) => {
-        let name = Date.now() + "-" + file.originalname
-        cb(null,name)
-    }
-})
+//     filename: (req, file, cb) => {
+//         let name = Date.now() + "-" + file.originalname
+//         cb(null,name)
+//     }
+// })
 ​
-const upload = multer({storage: storage});
+// const upload = multer({storage: storage});
 
 const homeRouter = require('./routes/home');
 const usersRouter = require('./routes/users');
@@ -40,16 +40,16 @@ app.use('/photos', photosRouter);
 app.use('/captions', captionsRouter);
 app.use('/likes', likesRouter);
 ​
-app.post('/upload', upload.single("image"), (req, res, next) => {
-    console.log('req.file', req.file)
-    console.log('req.body',req.body) 
+// app.post('/upload', upload.single("image"), (req, res, next) => {
+//     console.log('req.file', req.file)
+//     console.log('req.body',req.body) 
 
-    let imageUrl = "http:localhost:3001/" + req.file.path.replace('public/', '')
+//     let imageUrl = "http:localhost:3001/" + req.file.path.replace('public/', '')
 
-    res.json({ 
-        imageUrl: imageUrl,
-        message: 'file recieved, image was uploaded to images folder.'
-    })
-})
-​
+//     res.json({ 
+//         imageUrl: imageUrl,
+//         message: 'file recieved, image was uploaded to images folder.'
+//     })
+// })
+// ​
 module.exports = app;
