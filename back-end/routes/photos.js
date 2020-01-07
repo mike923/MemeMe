1ab1ceeb08ee25d1dd692d958c41e48468bea5c6
Owen Jones <owenthefirst93@gmail.com>
2020-01-06 22:39:35 -0500

    done photos route

--- back-end/routes/photos.js
+++ back-end/routes/photos.js
@@ -3,6 +3,7 @@ const express = require('express');
 const db = require('./db.js')
 const router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
   console.log('Getting all photos. Please standby')
@@ -24,9 +25,9 @@ router.get('/', async (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
   console.log('Getting photo by id. Please standby')
  
-  const {id} = parseInt(req.params)
+  const {id} = req.params
   try{
-    const photos = await db.none(`SELECT * FROM photos WHERE id = $1`, [id])
+    const photos = await db.one(`SELECT * FROM photos WHERE id = $1`, [id])
     res.json ({
       payload: photos,
       message: 'Success getting photo',
@@ -41,17 +42,39 @@ router.get('/:id', async (req, res, next) => {
   }
 })
 
-// router.post('/')
-// router.patch('/')
+router.post('/', async(req, res, next) => {
+  const {posterId, pictureUrl, datePosted, active} = req.body
+  const inputQuery = (`INSERT INTO photos (poster_id, picture_url,date_posted, active) VALUES($1, $2, $3, $4) `)
+  console.log('Adding photo')
+  try {
+      await db.one(inputQuery,[posterId, pictureUrl, datePosted, active])
+      res.json({
+          message:'Success. Photo posted',
+          payload: req.body,
+          success: true
+      })
 
-router.delete('/:id', async (req, res, next) => {
-  console.log('Deleting photo by id. Please standby')
+  } catch(error){
+      res.json({
+          message: 'Failed to add comment to post',
+          success: false
+      })
+      console.log(error)
+  }
+})
+
+
+router.patch('/:id', async (req, res, next) => {
+  console.log('Inactivating photo by id. Please standby')
   const {id} = req.params
+  const {active} = req.body
+  console.log(req.body)
   try{
-     await db.one('DELETE FROM photos WHERE id = $1' [id])
+     await db.none(`UPDATE photos SET active = $1 WHERE id= $2`, [false, id])
     res.json ({
       message: 'Success removing photo',
-      success: true
+      success: true,
+      payload: active
     }) 
   } catch (error) {
     res.json({
