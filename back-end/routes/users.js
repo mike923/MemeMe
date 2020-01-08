63c7a74257872f0b1a61fc466877fd172b9ce04d
Owen Jones <owenthefirst93@gmail.com>
2020-01-07 20:32:33 -0500

    user route

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -8,10 +8,10 @@ const db = require('./db')
 
 
 
-router.get('/all', async (req, res) => {
+router.get('/all/active', async (req, res) => {
     console.log('running');
     try {
-        let users = await db.any('SELECT * FROM users')
+        let users = await db.any('SELECT * FROM users WHERE active = true')
         res.json({
             payload: users,
             message: "Succcess, Retrieved all the users"
@@ -24,31 +24,72 @@ router.get('/all', async (req, res) => {
     }
 })
 
+router.get('/all/inactive', async (req, res) => {
+    console.log('running');
+    try {
+        let users = await db.any('SELECT * FROM users WHERE active = false')
+        res.json({
+            payload: users,
+            message: "Succcess, Retrieved all inactive the users"
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({
+            message: 'error something went wrong. Could not retrieve all users.'
+        })
+    }
+})
 
-router.get('/:displaname', async (req, res)=>{
-    let displayName = req.params.firstName;
+
+router.get('/:displayname', async (req, res)=>{
+    let {displayname} = req.params
     
-try {
-    let userQuery = `SELECT * FROM users WHERE displayname = $1`;
-   let user = await db.one(userQuery, [displayname]);
-   console.log(user);
-   if(user.length === 0){
-    throw new Error;
-   }
+    try {
+        let userQuery = `SELECT * FROM users WHERE displayname = $1 AND active = true`;
+        let user = await db.one(userQuery, [displayname]);
+        console.log(user);
+        if(user.length === 0){
+            throw new Error;
+        }
 
-    res.json({
-        payload: user,
-        message: "One USER received"
-    })
+        res.json({
+            payload: user,
+            message: "One USER received"
+        })
 
-} 
- catch (error){
-   console.log(error)
-   res.json({"err": "This user does not exist"});
-}
+    } catch (error){
+        console.log(error)
+        res.json({"err": "This user does not exist"});
+    }
 });
 
+router.patch('/:user_id', async (req, res) => {
+    let setQ = ''
+    for (key in req.body) {
+        let set = `${key} = '${req.body[key]}'`
+        setQ += set + ','
+    }
+    setQ = setQ.slice(0, setQ.length - 1)
+    
+    let insertQuery = `
+        UPDATE users
+        SET ${setQ}
+        WHERE id = '${req.params.user_id}'
+    `
 
+    try {
+        await db.none(insertQuery)
+        res.json({
+            username: `${req.params.user_id}`,
+            changes: req.body,
+        })
+    } catch(error) {
+        res.json({
+            message: 'There was an error',
+            error,
+        })
+    }
+})
 
 
 
@@ -75,24 +116,44 @@ router.post('/signup', async  (req, res) => {
     }
 })
 
-
-
-
-router.delete('/:user_id', async (req, res)=>{
-let userId = req.params.user_id 
-
-try{
-    let deleteUser =   `DELETE FROM users where id = $1`
-    await db.any(deleteUser, [userId])
-    res.json({
-        message: "This User  was Deleted"
+router.patch('/deactivate/:user_id', async (req, res)=>{
+    let {user_id} = req.params
+    
+    try{
+        let editUser = `
+            UPDATE users 
+            SET active = false
+            WHERE id = '${user_id}'
+        `
+        console.log(editUser)
+        await db.none(editUser)
+        res.json({
+            message: "Profile deactivated"
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({
+            message: "There was an error deactivating user",
+            error,
+        })
+    }
     })
-} catch (error) {
-    res.json({
-        message: "There was an error deleting user"
-    })
-}
-})
+
+// router.delete('/:user_id', async (req, res)=>{
+// let userId = req.params.user_id 
+
+// try{
+//     let deleteUser =   `DELETE FROM users where id = $1`
+//     await db.any(deleteUser, [userId])
+//     res.json({
+//         message: "This User  was Deleted"
+//     })
+// } catch (error) {
+//     res.json({
+//         message: "There was an error deleting user"
+//     })
+// }
+// })
 
 
 module.exports = router;
