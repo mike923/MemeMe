8d0ccc4fa7a233c0daddc6ab8410dff46fe588a9
aransagarcia <aransagarcia@pursuit.org>
2020-01-08 19:42:13 -0500

    working on login button

--- front-end/src/Components/Landing.jsx
+++ front-end/src/Components/Landing.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+
 
 class Landing extends Component {
     constructor() {
@@ -11,12 +13,20 @@ class Landing extends Component {
     }
 
     // submitForm is the function used to handle the onSubmit event for the form in the render 
-    submitForm = (event) => {
+    submitForm = async (event) => {
         event.preventDefault();
 
         console.log(event.target)
         console.log(this.state)
         console.log('here would go our network request to create a new user')
+
+        let loginURL = 'http://localhost:3001/users/all/active'
+       try {
+        let response = await axios.get(loginURL)
+        console.log(response)
+       } catch (error){
+           console.log(error, 'axios not working')
+       }
     }
 
 
@@ -35,7 +45,7 @@ class Landing extends Component {
         } = this
 
         return (
-            <form>
+            <form onSubmit ={this.submitForm}>
                 <h2>Landing page</h2>
                 <label htmlFor="email">email: </label>
                 <input 
