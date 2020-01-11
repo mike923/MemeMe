1b41a3a2c5a84b569c8b0c5b9ef655ca0b92024e
Owen Jones <owenthefirst93@gmail.com>
2020-01-11 13:16:32 -0500

    photo upload timesatmp

--- front-end/src/Components/PhotoUpload.jsx
+++ front-end/src/Components/PhotoUpload.jsx
@@ -5,19 +5,24 @@ class PhotoUpload extends React.Component {
     constructor(){
         super()
         this.state = {
-            user_id: '',
+            user_id: 2,
             imageFile: null,
-            imgURL: ''
+            imgURL: '',
+            // date_posted: new Date()
         }
     }  
 
-    handleUserProfile = (event) => {
-
-    }
+   
+    // handleDatePosted = (event) => {
+    //     const newDate = Date.now()
+    //     this.setState({
+    //         date_posted: newDate
+    //     })
+    //     console.log(newDate)
+    // }
     handleFileInput = (event) => {
         console.log('file changed')
         console.dir(event.target)
-        // console.log(event.target.files[0].lastModified + '-' + event.target.files[0].name)
         console.log(event.target.files[0])
         this.setState ({
             imageFile: event.target.files[0]
@@ -26,8 +31,16 @@ class PhotoUpload extends React.Component {
 
     handleSubmit = async (event) => {
         event.preventDefault()
+        const {user_id} = this.state
+        const data = new FormData()
+        data.append('imgURL', this.state.imageFile)
+        // data.append('date_posted', this.state.date_posted)
         try{
-            const response = await axios.post('http://localhost:3001/photos/upload')
+            const response = await axios.post(`http://localhost:3001/photos/upload/${user_id}`, data)
+            console.log(response.data)
+            this.setState ({
+                imgURL: response.data,
+            })
         } catch(error) {
             console.log('err', error)
         }
@@ -36,9 +49,9 @@ class PhotoUpload extends React.Component {
     render() {
         return (
             <div className ='uploader'>
-                <form>
-                    <input type='file' onChange={this.handleFileInput}/>
-                    <input type='submit' value='Upload'/>
+                <form onSubmit={this.handleSubmit} >
+                    <input type='file' onChange={this.handleFileInput} />
+                    <input type='submit' value='Upload' />
                 </form>
             </div>
         )
