8c5b64efd8e5353f89c206bf54dfca07ce005c91
MikePursuit <michaelamparo@pursuit.org>
2020-01-11 12:55:56 -0500

    populating user profile with request

--- front-end/src/Components/UserProfile.jsx
+++ front-end/src/Components/UserProfile.jsx
@@ -1,61 +1,68 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 
 class UserProfile extends Component {
     constructor() {
         super()
         this.initialState = {
+            id: 1,
             bio: 'this is my bio its not much but it will be soon.',
-            fullname: 'fullname',
-            username: 'username',
-            profilePicUrl: 'https://nwsid.net/wp-content/uploads/2015/05/dummy-profile-pic.png',
+            firstname: 'firstname',
+            displayname: 'displayname',
+            profilepic: 'https://nwsid.net/wp-content/uploads/2015/05/dummy-profile-pic.png',
             profilePicAlt: 'profile picture',
         }
         this.state = this.initialState
     }
 
+    componentDidMount = async (id = 2) => {
+        let {data: {user}} = await axios.get(`http://localhost:3001/users/${id}`)
+        console.log(user)
+        this.setState(user)
+    }
 
 
     render() {
         let {
             state: {
                 bio,
-                username,
-                fullname,
+                displayname,
+                firstname,
                 profilePicAlt,
-                profilePicUrl,
+                profilepic,
             },
         } = this
 
         return(
             <div>
                 <div>
-                    <h2>{username}</h2>
-                    <img src={profilePicUrl} alt={profilePicAlt} height="150px"/>
-                    <h3>{fullname}</h3>
+                    <h2>{displayname}</h2>
+                    <img src={profilepic} alt={profilePicAlt} height="150px"/>
+                    <h3>{firstname}</h3>
                     <p>{bio}</p>
                 </div>
                 <div>
                     <h3>My photos go here</h3>
                     <div>
-                        <img src={profilePicUrl} alt={profilePicAlt} height="150px"/>
-                        <img src={profilePicUrl} alt={profilePicAlt} height="150px"/>
-                        <img src={profilePicUrl} alt={profilePicAlt} height="150px"/>
+                        <img src={profilepic} alt={profilePicAlt} height="150px"/>
+                        <img src={profilepic} alt={profilePicAlt} height="150px"/>
+                        <img src={profilepic} alt={profilePicAlt} height="150px"/>
                     </div>
                     <div>
-                        <img src={profilePicUrl} alt={profilePicAlt} height="150px"/>
-                        <img src={profilePicUrl} alt={profilePicAlt} height="150px"/>
-                        <img src={profilePicUrl} alt={profilePicAlt} height="150px"/>
+                        <img src={profilepic} alt={profilePicAlt} height="150px"/>
+                        <img src={profilepic} alt={profilePicAlt} height="150px"/>
+                        <img src={profilepic} alt={profilePicAlt} height="150px"/>
                     </div>
                     <div>
-                        <img src={profilePicUrl} alt={profilePicAlt} height="150px"/>
-                        <img src={profilePicUrl} alt={profilePicAlt} height="150px"/>
-                        <img src={profilePicUrl} alt={profilePicAlt} height="150px"/>
+                        <img src={profilepic} alt={profilePicAlt} height="150px"/>
+                        <img src={profilepic} alt={profilePicAlt} height="150px"/>
+                        <img src={profilepic} alt={profilePicAlt} height="150px"/>
                     </div>
                     <div>
-                        <img src={profilePicUrl} alt={profilePicAlt} height="150px"/>
-                        <img src={profilePicUrl} alt={profilePicAlt} height="150px"/>
-                        <img src={profilePicUrl} alt={profilePicAlt} height="150px"/>
+                        <img src={profilepic} alt={profilePicAlt} height="150px"/>
+                        <img src={profilepic} alt={profilePicAlt} height="150px"/>
+                        <img src={profilepic} alt={profilePicAlt} height="150px"/>
                     </div>
                 </div>
             </div>
