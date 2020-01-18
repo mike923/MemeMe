eb024af7a4f71ede4d404fbc9ee269854a0e6583
MikePursuit <michaelamparo@pursuit.org>
2020-01-18 13:26:26 -0500

    caption inputs work

--- front-end/src/Components/InputCaption.jsx
+++ front-end/src/Components/InputCaption.jsx
@@ -1,26 +1,43 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 import axios from 'axios'
 
 class InputCaption extends Component {
     constructor(props) {
         super(props) 
-        this.state = {
-            caption: ''
+        this.initialState = {
+            caption: '',
         }
+        this.state = this.initialState
     }
 
     handleInput = ({target: {value}}) => this.setState({caption: value})
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault()
+        console.log(this.props.id)
+        try {
+            let {data} = await axios.post(`http://localhost:3001/captions/photos`, {photo_id: this.props.id, commenter_id: this.state.id, body:this.state.caption})
+            console.log(data)
+            this.setState({caption:''})
+            this.props.load()
 
-        console.log(this.state.caption)
+        } catch(error) {
+            console.log(error)
+        }
     }
-
-    inputCaption = () => {
-        const {caption} = this.state
+    
+    componentDidMount = async () => {
+        let id = sessionStorage.getItem('id')
+        try {
+            let {data:{user}} = await axios.get(`http://localhost:3001/users/${id}`)
+            console.log(user)
+            this.setState(user)
+        } catch(error) {
+            console.log(error)
+        }
     }
-
+    
     render() {
         let {
             state: {
@@ -31,21 +48,19 @@ class InputCaption extends Component {
         } = this
 
         return(
-            <div class="row">
-                <form class="col s12">
-                    <div class="row">
-                        <div class="input-field col s6">
-                            <i class="material-icons prefix">mode_edit</i>
-                            <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
-                            <label for="icon_prefix2">First Name</label>
-                        </div>
+            <div className='container' >
+            <div className="row">
+                <form className="collection col s12" onSubmit={handleSubmit}>
+                    <div className="ipnut-field collection-item avatar row" style={{textAlign: 'left'}}>
+                        <Link to={'/user/' + this.state.displayname}>
+                            <img src={this.state.profilepic} alt="" className="circle"  />
+                        </Link>
+                        <span className="title" style={{textAlign: 'left'}}>{this.state.firstname}</span>
+                        <input value={caption} onChange={handleInput} id="icon_prefix2" placeholder="write your caption here" className="materialize-textarea" />
                     </div>
                 </form>
             </div>
-
-            // <form onSubmit={handleSubmit}>
-            //     <input type="text" value={caption} onChange={handleInput}/>
-            // </form>
+            </div>
         )
     }
 }
