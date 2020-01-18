aa89c0deade2040806a2b39b604df5e38d7f600e
aransagarcia <aransagarcia@pursuit.org>
2020-01-18 11:46:42 -0500

    added toastify to photo upload

--- front-end/src/Components/NavBar.jsx
+++ front-end/src/Components/NavBar.jsx
@@ -6,20 +6,20 @@ const NavBar = () => {
         <nav className="NavBar">
               <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
              
-            <Link to="/post"
-            > post  </Link>
+            {/* <Link to="/post"
+            > post  </Link> */}
             <br />
             <Link to="/feed"> <i class="large material-icons"
             >explore</i> </Link>
             <br />
-            <Link to="/photos/upload"><i class="large material-icons"
-            >add_a_photo</i></Link>
+            {/* <Link to="/photos/upload"><i class="large material-icons"
+            >add_a_photo</i></Link> */}
             <br />
             <Link to="/user"><i class="large material-icons"
             > face </i></Link>
             <br />
-            <Link to="/EditProfile"> <i class="large material-icons"
-            >edit</i></Link>
+            {/* <Link to="/EditProfile"> <i class="large material-icons"
+            >edit</i></Link> */}
             <br/>
             <Link to="/LogOut"> <i class="large material-icons"
             >exit_to_app</i></Link>
