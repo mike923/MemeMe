f0c39dc487a904ceec420d91a06732a1d7f580e2
Owen Jones <owenthefirst93@gmail.com>
2020-11-01 18:44:15 -0500

    _dir name fix

--- back-end/app.js
+++ back-end/app.js
@@ -31,7 +31,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(_dirname, '../front-end/build')))
+app.use(express.static(path.join(__dirname, '../front-end/build')))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/sessions', sessionsRouter);
