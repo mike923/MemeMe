9b95f53d1574820301481236708a4488df010536
Owen Jones <owenthefirst93@gmail.com>
2020-11-21 16:47:34 -0500

    apiUrl fix and proxy add

--- newfrontend/src/apiURL.js
+++ newfrontend/src/apiURL.js
@@ -2,7 +2,7 @@ const getAPIURL = () => {
     if(window.location.hostname === 'localhost'){
         return `http://localhost:3001`
     }
-    return `https://aqueous-sierra-27538.herokuapp.com/`
+    return `https://aqueous-sierra-27538.herokuapp.com`
 }
 
 
